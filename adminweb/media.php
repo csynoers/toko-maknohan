e89f01933c6129120d3e4ec94e8a29334ecb1c9e
csynoers <3s0c9m7@gmail.com>
2020-01-08 19:27:57 +0700

    clear trash

--- adminweb/media.php
+++ adminweb/media.php
@@ -36,68 +36,70 @@ else{
   <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
   <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
   <![endif]-->
-  <script type="text/javascript" src="js/ckeditor/ckeditor.js"></script>
-	<script language="javascript" type="text/javascript">
-    tinyMCE_GZ.init({
-    plugins : 'style,layer,table,save,advhr,advimage, ...',
-		themes  : 'simple,advanced',
-		languages : 'en',
-		disk_cache : true,
-		debug : false
-});
-</script>
-<script language="javascript" type="text/javascript"
-src="../tinymcpuk/tiny_mce_src.js"></script>
-<script type="text/javascript">
-tinyMCE.init({
-    width : '100%',
-		mode : "textareas",
-		theme : "advanced",
-		plugins : "table,youtube,advhr,advimage,advlink,emotions,flash,searchreplace,paste,directionality,noneditable,contextmenu",
-		theme_advanced_buttons1_add : "fontselect,fontsizeselect",
-		theme_advanced_buttons2_add : "separator,preview,zoom,separator,forecolor,backcolor,liststyle",
-		theme_advanced_buttons2_add_before: "cut,copy,paste,separator,search,replace,separator",
-		theme_advanced_buttons3_add_before : "tablecontrols,separator,youtube,separator",
-		theme_advanced_buttons3_add : "emotions,flash",
-		theme_advanced_toolbar_location : "top",
-		theme_advanced_toolbar_align : "left",
-		theme_advanced_statusbar_location : "bottom",
-		extended_valid_elements : "hr[class|width|size|noshade]",
-		file_browser_callback : "fileBrowserCallBack",
-		paste_use_dialog : false,
-		theme_advanced_resizing : true,
-		theme_advanced_resize_horizontal : false,
-		theme_advanced_link_targets : "_something=My somthing;_something2=My somthing2;_something3=My somthing3;",
-		apply_source_formatting : true
-});
 
-	function fileBrowserCallBack(field_name, url, type, win) {
-		var connector = "../../filemanager/browser.html?Connector=connectors/php/connector.php";
-		var enableAutoTypeSelection = true;
-		
-		var cType;
-		tinymcpuk_field = field_name;
-		tinymcpuk = win;
-		
-		switch (type) {
-			case "image":
-				cType = "Image";
-				break;
-			case "flash":
-				cType = "Flash";
-				break;
-			case "file":
-				cType = "File";
-				break;
-		}
-		
-		if (enableAutoTypeSelection && cType) {
-			connector += "&Type=" + cType;
-		}
-		
-		window.open(connector, "tinymcpuk", "modal,width=600,height=400");
-	}
-</script>
+    <!-- <script type="text/javascript" src="js/ckeditor/ckeditor.js"></script> -->
+	<!-- <script language="javascript" type="text/javascript">
+        tinyMCE_GZ.init({
+            plugins : 'style,layer,table,save,advhr,advimage, ...',
+                themes  : 'simple,advanced',
+                languages : 'en',
+                disk_cache : true,
+                debug : false
+        });
+    </script> -->
+
+    <script language="javascript" type="text/javascript" src="../tinymcpuk/tiny_mce_src.js"></script>
+    <script type="text/javascript">
+        tinyMCE.init({
+            width : '100%',
+                mode : "textareas",
+                theme : "advanced",
+                plugins : "table,youtube,advhr,advimage,advlink,emotions,flash,searchreplace,paste,directionality,noneditable,contextmenu",
+                theme_advanced_buttons1_add : "fontselect,fontsizeselect",
+                theme_advanced_buttons2_add : "separator,preview,zoom,separator,forecolor,backcolor,liststyle",
+                theme_advanced_buttons2_add_before: "cut,copy,paste,separator,search,replace,separator",
+                theme_advanced_buttons3_add_before : "tablecontrols,separator,youtube,separator",
+                theme_advanced_buttons3_add : "emotions,flash",
+                theme_advanced_toolbar_location : "top",
+                theme_advanced_toolbar_align : "left",
+                theme_advanced_statusbar_location : "bottom",
+                extended_valid_elements : "hr[class|width|size|noshade]",
+                file_browser_callback : "fileBrowserCallBack",
+                paste_use_dialog : false,
+                theme_advanced_resizing : true,
+                theme_advanced_resize_horizontal : false,
+                theme_advanced_link_targets : "_something=My somthing;_something2=My somthing2;_something3=My somthing3;",
+                apply_source_formatting : true
+        });
+
+
+        function fileBrowserCallBack(field_name, url, type, win) {
+            var connector = "../../filemanager/browser.html?Connector=connectors/php/connector.php";
+            var enableAutoTypeSelection = true;
+            
+            var cType;
+            tinymcpuk_field = field_name;
+            tinymcpuk = win;
+            
+            switch (type) {
+                case "image":
+                    cType = "Image";
+                    break;
+                case "flash":
+                    cType = "Flash";
+                    break;
+                case "file":
+                    cType = "File";
+                    break;
+            }
+            
+            if (enableAutoTypeSelection && cType) {
+                connector += "&Type=" + cType;
+            }
+            
+            window.open(connector, "tinymcpuk", "modal,width=600,height=400");
+        }
+    </script>
 </head>
 <body class="hold-transition skin-blue sidebar-mini">
 <div class="wrapper">
@@ -179,7 +181,7 @@ tinyMCE.init({
     <!-- Content Header (Page header) -->
     <section class="content-header">
       <h1>
-        Administrator E-Commerce Toko Sinar Abadi Batik
+        Administrator E-Commerce Toko Maknohan
         
       </h1>
       
@@ -202,7 +204,7 @@ tinyMCE.init({
     <div class="pull-right hidden-xs">
       <b>Version</b> 2.3.3
     </div>
-    <strong>Copyright &copy; 2018 Toko Sinar Abadi Batik All rights
+    <strong>Copyright &copy; <?= date('Y') ?> Toko Maknohan All rights
     reserved.
   </footer>
 
