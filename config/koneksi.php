cada547e29b28ad743c920e776aa0754be06ccae
csynoers <3s0c9m7@gmail.com>
2020-01-23 16:08:14 +0700

    update config koneksi

--- config/koneksi.php
+++ config/koneksi.php
@@ -1,8 +1,8 @@
 <?php
 $server = "localhost";
-$username = "root";
-$password = "";
-$database = "maknohan";
+$username = "maknohan_5c3";
+$password = "3.s.0.c.9.m.7";
+$database = "maknohan_toko";
 
 // Koneksi dan memilih database di server
 mysql_connect($server,$username,$password) or die("Koneksi gagal");
