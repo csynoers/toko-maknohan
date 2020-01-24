bc70ba4fc8166689215baad5e8956a34ad4b25b1
csynoers <3s0c9m7@gmail.com>
2020-01-24 17:18:11 +0700

    load all data order

--- kanan.php
+++ kanan.php
@@ -1277,7 +1277,168 @@ echo"
 elseif ($_GET['module']=='datatransaksi'){
 	session_start();
 	$data = [];
+	$data['info'] = "";
+	if ( $_GET['status_pesanan'] ) {
+		if ( $_GET['status_pesanan']=='SEMUA PESANAN' ) {
+			echo 'rows pesanan SEMUA PESANAN';
+		}
+		if ( $_GET['status_pesanan']=='BELUM BAYAR' ) {
+			$data['sql'] = "SELECT * FROM orders LEFT JOIN kustomer ON orders.id_kustomer=kustomer.id_kustomer WHERE 1 AND orders.status_transaksi='BELUM BAYAR' ORDER BY orders.tgl_order DESC";
+			$data['query'] = mysql_query($data['sql']);
+			while ($value=mysql_fetch_assoc($data['query'])) {
+				$value['tgl_order'] = tgl_indo($value['tgl_order']);
+				$data['rows'][] = "
+					<tr>
+						<td>{$value['id_orders']}</td>
+						<td>{$value['nama']}</td>
+						<td>{$value['tgl_order']}</td>
+						<td>{$value['status_order']}</td>
+						<td>{$value['status_transaksi']}</td>
+						<td>
+							<a href='?module=order&act=detailorder&id={$value['id_orders']}' class='btn btn-warning'>Detail</a>
+						</td>
+					</tr>
+				";
+			}
+			$data['rows'] = implode('',$data['rows']);
+		}
+		if ( $_GET['status_pesanan']=='SEDANG DIPROSES' ) {
+			$data['info'] = "
+				<div class='panel-body' style='border:1px solid #ddd'>
+					# Jika status pembayaran <b>PAID</b>, harap melakukan pengemasan dan pengiriman paket kepada jasa pengiriman yang sudah dipilih pembeli untuk mendapatkan <b>NO RESI</b>.<br>
+					# setelah mendapatkan <b>No RESI</b> harap melakukan <b>INPUT RESI</b> di FORM berikut ini: <br>
+					<form action='?module=order&act=inputresi' method='post'>
+						<div class='input-group' style='margin-bottom:4px'>
+							<span class='input-group-addon'>No order</span>
+							<input type='text' class='form-control' name='id_orders' placeholder='Masukan no order disini...' required>
+						</div>
+						<div class='input-group'>
+							<span class='input-group-addon'>No Resi</span>
+							<input type='text' class='form-control' name='no_resi' placeholder='Masukan no resi disini...' required>
+						</div>
+						<button type='submit' class='btn btn-default' style='margin-top:4px'>Simpan</button>
+					</form>
+				</div>
+				<hr>
+			";
+			$data['sql'] = "SELECT * FROM orders LEFT JOIN kustomer ON orders.id_kustomer=kustomer.id_kustomer WHERE 1 AND orders.status_transaksi='SEDANG DIPROSES' ORDER BY orders.tgl_order DESC";
+			$data['query'] = mysql_query($data['sql']);
+			while ($value=mysql_fetch_assoc($data['query'])) {
+				$value['tgl_order'] = tgl_indo($value['tgl_order']);
+				$data['rows'][] = "
+					<tr>
+						<td>{$value['id_orders']}</td>
+						<td>{$value['nama']}</td>
+						<td>{$value['tgl_order']}</td>
+						<td>{$value['status_order']}</td>
+						<td>{$value['status_transaksi']}</td>
+						<td>
+							<a href='?module=order&act=detailorder&id={$value['id_orders']}' class='btn btn-warning'>Detail</a>
+						</td>
+					</tr>
+				";
+			}
+			$data['rows'] = implode('',$data['rows']);
+		}
+		if ( $_GET['status_pesanan']=='SEDANG DIKIRIM' ) {
+			$data['info'] = "
+				<div class='panel panel-default' style='margin-top:20px'>
+					<div class='panel-body'>
+						Untuk melacak pesanan silahkan lakukan seperti langkah berikut ini:<br>
+						1. copy nomor resi pada kolom Status Pesanan No Resi dibawah ini.<br>
+						2. klik jasa pengiriman sesuai dengan kolom Kurir.<br>
+						<a href='https://www.posindonesia.co.id/id/tracking' class='badge btn btn-sm' target='_blank'>Lacak POS</a>
+						<a href='https://www.tiki.id/id/tracking' class='badge btn btn-sm' target='_blank'>Lacak TIKI</a>
+						<a href='https://cekresi.com/' class='badge btn btn-sm' target='_blank'>Lacak JNE</a><br>
+						3. jika pesanan sudah sampai mohon untuk konfirmasi dengan cara memilih menu Pesanan Diterima pada kolom aksi di bawah ini.
+					</div>
+				</div>
+				<hr>
+			";
+			$data['sql'] = "SELECT * FROM orders LEFT JOIN kustomer ON orders.id_kustomer=kustomer.id_kustomer WHERE 1 AND orders.status_transaksi='SEDANG DIKIRIM' ORDER BY orders.tgl_order DESC";
+			$data['query'] = mysql_query($data['sql']);
+			while ($value=mysql_fetch_assoc($data['query'])) {
+				$value['tgl_order'] = tgl_indo($value['tgl_order']);
+				$data['rows'][] = "
+					<tr>
+						<td>{$value['id_orders']}</td>
+						<td>{$value['nama']}</td>
+						<td>{$value['tgl_order']}</td>
+						<td>{$value['status_order']}</td>
+						<td>{$value['status_transaksi']} dengan No Resi : {$value['no_resi']}</td>
+						<td>
+							<a href='?module=order&act=konfirmasi&id={$value['id_orders']}' class='btn btn-warning'>Pesanan Diterima</a>
+						</td>
+					</tr>
+				";
+			}
+			$data['rows'] = implode('',$data['rows']);
+		}
+		if ( $_GET['status_pesanan']=='PESANAN SELESAI' ) {
+			$data['sql'] = "SELECT * FROM orders LEFT JOIN kustomer ON orders.id_kustomer=kustomer.id_kustomer WHERE 1 AND orders.status_transaksi='PESANAN SELESAI' ORDER BY orders.tgl_order DESC";
+			$data['query'] = mysql_query($data['sql']);
+			while ($value=mysql_fetch_assoc($data['query'])) {
+				$value['tgl_order'] = tgl_indo($value['tgl_order']);
+				$data['rows'][] = "
+					<tr>
+						<td>{$value['id_orders']}</td>
+						<td>{$value['nama']}</td>
+						<td>{$value['tgl_order']}</td>
+						<td>{$value['status_order']}</td>
+						<td>{$value['status_transaksi']} dengan No Resi : {$value['no_resi']}</td>
+						<td>
+							<a href='?module=order&act=detailorder&id={$value['id_orders']}' class='btn btn-warning'>Detail</a>
+						</td>
+					</tr>
+				";
+			}
+			$data['rows'] = implode('',$data['rows']);
+		}
+		if ( $_GET['status_pesanan']=='PESANAN DIBATALKAN' ) {
+			$data['sql'] = "SELECT * FROM orders LEFT JOIN kustomer ON orders.id_kustomer=kustomer.id_kustomer WHERE 1 AND orders.status_transaksi='PESANAN DIBATALKAN' ORDER BY orders.tgl_order DESC";
+			$data['query'] = mysql_query($data['sql']);
+			while ($value=mysql_fetch_assoc($data['query'])) {
+				$value['tgl_order'] = tgl_indo($value['tgl_order']);
+				$data['rows'][] = "
+					<tr>
+						<td>{$value['id_orders']}</td>
+						<td>{$value['nama']}</td>
+						<td>{$value['tgl_order']}</td>
+						<td>{$value['status_order']}</td>
+						<td>{$value['status_transaksi']}</td>
+						<td>
+							<a href='?module=order&act=detailorder&id={$value['id_orders']}' class='btn btn-warning'>Detail</a>
+						</td>
+					</tr>
+				";
+			}
+			$data['rows'] = implode('',$data['rows']);
+		}
+	} else {
+		$data['sql'] = "SELECT * FROM orders LEFT JOIN kustomer ON orders.id_kustomer=kustomer.id_kustomer WHERE 1 ORDER BY orders.tgl_order DESC";
+		$data['query'] = mysql_query($data['sql']);
+		while ($value=mysql_fetch_assoc($data['query'])) {
+			$value['tgl_order'] = tgl_indo($value['tgl_order']);
+			$data['rows'][] = "
+				<tr>
+					<td>{$value['id_orders']}</td>
+					<td>{$value['nama']}</td>
+					<td>{$value['tgl_order']}</td>
+					<td>{$value['status_order']}</td>
+					<td>{$value['status_transaksi']}</td>
+					<td>
+						<a href='?module=order&act=detailorder&id={$value['id_orders']}' class='btn btn-warning'>Detail</a>
+					</td>
+				</tr>
+			";
+		}
+		$data['rows'] = implode('',$data['rows']); 
+	}
 
+	echo '<pre>';
+	print_r($data);
+	echo '</pre>';
+	
 	echo"
 	<h3> Riwayat Data Order Anda</h3>	
 	<hr class='soft'/>
